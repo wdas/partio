46d55f1ad429b828cab57e502fab903c8e83e89d
Pal Mezei <sirpalee@gmail.com>
2017-03-15 10:45:16 +1100

    Adding an arnold 5 compatible partio procedural.

--- contrib/cmake/FindArnold.cmake
+++ contrib/cmake/FindArnold.cmake
@@ -1,26 +1,50 @@
-# - Arnold finder module
-# This module searches for a valid Arnold instalation.
-#
-# Variables that will be defined:
-# ARNOLD_FOUND             Defined if a Arnold installation has been detected
-# ARNOLD_LIBRARY           Path to ai library
-# ARNOLD_INCLUDE_DIR       Path to the include directory
-#
-# Naming convention:
-#  Local variables of the form _arnold_foo
-#  Input variables from CMake of the form Arnold_FOO
-#  Output variables of the form ARNOLD_FOO
-#
-
 find_library(ARNOLD_LIBRARY
-    NAMES ai
-    PATHS $ENV{ARNOLD_HOME}/bin
-    DOC "Arnold library")
+        NAMES ai
+        PATHS $ENV{ARNOLD_HOME}/bin
+        DOC "Arnold library")
+
+find_file(ARNOLD_KICK
+        names kick
+        PATHS $ENV{ARNOLD_HOME}/bin
+        DOC "Arnold kick executable")
+
+find_file(ARNOLD_PYKICK
+        names pykick
+        PATHS $ENV{ARNOLD_HOME}/python/pykikc
+        DOC "Arnold pykick executable")
+
+find_file(ARNOLD_MAKETX
+        names maketx
+        PATHS $ENV{ARNOLD_HOME}/bin
+        DOC "Arnold maketx executable")
 
 find_path(ARNOLD_INCLUDE_DIR ai.h
-    PATHS $ENV{ARNOLD_HOME}/include
-    DOC "Arnold include path")
+        PATHS $ENV{ARNOLD_HOME}/include
+        DOC "Arnold include path")
+
+find_path(ARNOLD_PYTHON_DIR arnold/ai_allocate.py
+        PATHS $ENV{ARNOLD_HOME}/python
+        DOC "Arnold python bindings path")
+
+if(ARNOLD_INCLUDE_DIR AND EXISTS "${ARNOLD_INCLUDE_DIR}/ai_version.h")
+    foreach(comp ARCH_NUM MAJOR_NUM MINOR_NUM FIX)
+        file(STRINGS
+                ${ARNOLD_INCLUDE_DIR}/ai_version.h
+                TMP
+                REGEX "#define AI_VERSION_${comp} .*$")
+        string(REGEX MATCHALL "[0-9]+" ARNOLD_VERSION_${comp} ${TMP})
+    endforeach()
+    set(ARNOLD_VERSION ${ARNOLD_VERSION_ARCH_NUM}.${ARNOLD_VERSION_MAJOR_NUM}.${ARNOLD_VERSION_MINOR_NUM}.${ARNOLD_VERSION_FIX})
+endif()
+
+message(STATUS "Arnold library: ${ARNOLD_LIBRARY}")
+message(STATUS "Arnold headers: ${ARNOLD_INCLUDE_DIR}")
+message(STATUS "Arnold version: ${ARNOLD_VERSION}")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Arnold DEFAULT_MSG
-    ARNOLD_LIBRARY ARNOLD_INCLUDE_DIR)
+find_package_handle_standard_args(Arnold
+        REQUIRED_VARS
+        ARNOLD_LIBRARY
+        ARNOLD_INCLUDE_DIR
+        VERSION_VAR
+        ARNOLD_VERSION)
